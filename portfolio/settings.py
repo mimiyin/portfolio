e161eaacec869057cf781000814a33cc7209db09
Mimi Yin <mimi@parlor.is>
2014-07-21 18:14:31 -0400

    Changing db engine.

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,7 +11,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'mysql.connector.django', #'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'portfolio', #/Users/hamstar/gitroot/portfolio/sqlite.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'capps',
@@ -166,4 +166,4 @@ LOGGING = {
     }
 }
 
-#import settings_local
+from settings_local import *
