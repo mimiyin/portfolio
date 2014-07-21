2b2c5221557e388823bc81549750208a67782637
Mimi Yin <mimi@parlor.is>
2014-07-21 19:15:19 -0400

    static files

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -64,7 +64,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -167,5 +167,3 @@ LOGGING = {
         },
     }
 }
-
-#from settings_local import *
